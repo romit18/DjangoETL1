efea3b9f2e89de623f5371210ca3fb57cc05492f
romit18 <46364956+romit18@users.noreply.github.com>
2019-09-22 17:59:56 +0530

    initial

---
+++ firstapp/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+
+from django.views.generic import CreateView
+from django.contrib.auth import login,authenticate,logout
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render,redirect
+
+from . import forms
+def index(request):
+  return render(request, 'index.html')
+
+def services(request):
+  return render(request,'services.html')
+
+def signup(request):
+  if request.method=="POST":
+    form = forms.UserCreateForm(request.POST)
+    if form.is_valid():
+      user=form.save()
+      username=form.cleaned_data.get("username")
+      email=form.cleaned_data.get("email")
+      login(request,user)
+      return redirect('index')
+    else:
+      for msg in form.error_messages:
+        print(form.error_messages[msg])
+
+      return render(request = request,
+                          template_name = "registration/signup.html",
+                          context={"form":form})
+  form=forms.UserCreateForm
+  return render(request,'registration/signup.html',{'form': form})
+
+  def login(request):
+    if request.method == 'POST': 
+        username = request.POST['username'] 
+        password = request.POST['password'] 
+        user = authenticate(request, username = username, password = password) 
+        if user is not None: 
+            form = login(request, user) 
+            messages.success(request, f' Welcome {username} !!') 
+            return redirect('index') 
+        else: 
+            messages.info(request, f'account done not exit please sign in') 
+    form = AuthenticationForm() 
+    return render(request, 'registration/login.html', {'form':form, 'title':'log in'}) 
